f84a9c5811c8a5b7533b2a14f8b8aff2e7ed3298
vaibsrin <vaibsrin@umich.edu>
2020-01-12 12:25:58 -0500

    Initial trial of csv validation

--- src/components/dropzone.stories.js
+++ src/components/dropzone.stories.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Dropzone from "./dropzone";
+import CSVFileValidator from 'csv-file-validator';
 
 export default {
     component: Dropzone,
@@ -7,3 +8,52 @@ export default {
 }
 
 export const dropZone = () => <Dropzone />
+
+CSVFileValidator(file, config)
+    .then(csvData => {
+        csvData.data // Array of objects from file
+        csvData.inValidMessages // Array of error messages
+    })
+    .catch(err => {})
+
+
+    const config = {
+        headers: [
+            {
+                name: 'Date',
+                inputName: 'date',
+                required: true,
+                requiredError: function (headerName, rowNumber, columnNumber) {
+                    return `${headerName} is required in the ${rowNumber} row / ${columnNumber} column`
+                }
+            },
+            {
+                name: 'Startup',
+                inputName: 'companyname',
+                requiredError: function (headerName, rowNumber, columnNumber) {
+                    return `${headerName} is required in the ${rowNumber} row / ${columnNumber} column`
+                }
+            },
+            {
+                name: 'Engagement',
+                inputName: 'engagement',
+                required: true,
+                requiredError: function (headerName, rowNumber, columnNumber) {
+                    return `${headerName} is required in the ${rowNumber} row / ${columnNumber} column`
+                }
+            },
+            {
+                name: 'Partner, Investor, Organization',
+                inputName: 'Investor',
+                required: true,
+                requiredError: function (headerName, rowNumber, columnNumber) {
+                    return `${headerName} is required in the ${rowNumber} row / ${columnNumber} column`
+                }
+            },
+            {
+            name: 'Notes',
+            inputName: 'notes', 
+            required: false
+            },
+        ]
+    }
